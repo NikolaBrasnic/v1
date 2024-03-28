9c9fe439959dc8a563a2dfc820db41fe100c9b93
Nikola Brasnic <nikolabrasnic@outlook.de>
2024-03-28 02:24:30 +0100

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,12 +60,11 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+      Ich bin stets auf der Suche nach neuen Herausforderungen und engagiere mich mit Begeisterung für die Umsetzung kreativer und interessanter Projekte.
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
-        Say Hello
+        Anfrage
       </a>
     </StyledContactSection>
   );
